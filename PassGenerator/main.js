d4d816d348f0ad1ff5ce2fbcaa9c0e324aeb9b14
L0o0k1 <ahmedomxo1@tutanota.com>
2024-08-14 05:29:54 +0300

    PassGenEditing

--- PassGenerator/main.js
+++ PassGenerator/main.js
@@ -1,31 +1,30 @@
 const passInp = document.querySelector(".passwordBox input"),
-cpICon = document.querySelector(".passwordBox .copy-icon"),
-rgInput = document.querySelector(".rangeBox input"),
-sldNum = document.querySelector(".rangeBox .slider-number"),
-gntBtn = document.querySelector(".generate-button");
+  cpICon = document.querySelector(".passwordBox .copy-icon"),
+  rgInput = document.querySelector(".rangeBox input"),
+  sldNum = document.querySelector(".rangeBox .slider-number"),
+  gntBtn = document.querySelector(".generate-button");
 //============================================
-let all_Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-const gen_Passwod = function (){
-    let N_pass = "";
+let all_Chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+const gen_Passwod = function () {
+  let N_pass = "";
 
-    for (let i = 0; i < rgInput.value; i++){
-        let randomNumNum = Math.floor(Math.random() * all_Chars.length);
-        N_pass += all_Chars[randomNumNum];
-    }
-    passInp.value = N_pass;
-    cpICon.classList.replace('uil-file-check-alt','uil-copy')
-
-}
+  for (let i = 0; i < rgInput.value; i++) {
+    let randomNumNum = Math.floor(Math.random() * all_Chars.length);
+    N_pass += all_Chars[randomNumNum];
+  }
+  passInp.value = N_pass;
+  cpICon.classList.replace("uil-file-check-alt", "uil-copy");
+};
 // ===========================================
 rgInput.addEventListener("input", () => {
-    sldNum.innerText = rgInput.value;
-    gen_Passwod();
+  sldNum.innerText = rgInput.value;
+  gen_Passwod();
 });
 // ===========================================
 cpICon.addEventListener("click", () => {
-    navigator.clipboard.writeText(passInp.value);
-    cpICon.classList.replace('uil-copy','uil-file-check-alt')
-
+  navigator.clipboard.writeText(passInp.value);
+  cpICon.classList.replace("uil-copy", "uil-file-check-alt");
 });
 gen_Passwod();
-gntBtn.addEventListener("click", gen_Passwod)
+gntBtn.addEventListener("click", gen_Passwod);
